80af124c2fdcab7e5ea61aea436efdc44baf0701
wangan <il>
2018-08-13 22:30:26 +0800

    add all

---
+++ README.md
@@ -0,0 +1,8 @@
+首先安装nodejs
+进入webpack-demo文件夹执行
+npm init 
+npm install webpack --save-dev
+npm install --save-dev ts-loader  style-loader
+npm install -g webpack
+最后执行
+webpack
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         hello: './src/script/hello.js',
         world: './src/script/world.js'
     },
-    
-    output:{
+
+    output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].js'
     },
     module: {
         rules: [
-          {
-            test: /\.css$/,
-            use: [
-              { loader: 'style-loader' },
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: true
-                }
-              }
-            ]
-          }
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
         ]
-      },
-      plugins: [
-        new HtmlWebpackPlugin({template: './src/index.html'})
-      ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index-[hash].html',
+            inject: 'head'
+        })
+    ]
 };
